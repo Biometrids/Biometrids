857be79f79b39103a54244ae2c8c8fb1f9d05e35
Kirill Beresnev <derainberk@gmail.com>
2017-11-08 15:57:16 +0300

    Added test to the test suite

--- run-tests.sh
+++ run-tests.sh
@@ -2,3 +2,4 @@
 
 ./node_modules/.bin/truffle test ./test/1_BiometridsToken.js
 ./node_modules/.bin/truffle test ./test/2_PreSalePricingStrategyTest.js
+./node_modules/.bin/truffle test ./test/3_IcoStagesPricingStrategy.js
