771fc18a8732928b596afa19c1c144aa9c8d2158
Kirill Beresnev <derainberk@gmail.com>
2017-11-10 18:13:27 +0300

    Added refund vault

---
+++ contracts/RefundVault.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.4.18;
+
+import "../node_modules/zeppelin-solidity/contracts/crowdsale/RefundVault.sol";
+
+/**
+ * @notice We use a fully tested impementation of refund vault from OpenZeppelin v1.3.0
+ * Code https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/contracts/crowdsale/RefundVault.sol
+ * Tests https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/test/RefundVault.js
+ */
+contract CrowdSaleRefundVault is RefundVault {
+}
