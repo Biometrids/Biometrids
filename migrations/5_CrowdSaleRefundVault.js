172ecfc53d6a0d85ce5971e59f76dae533d2caa1
Kirill Beresnev <derainberk@gmail.com>
2017-11-25 04:24:34 +0300

    Updated migration comments

--- migrations/5_CrowdSaleRefundVault.js
+++ migrations/5_CrowdSaleRefundVault.js
@@ -2,7 +2,6 @@ const CrowdSaleRefundVault = artifacts.require('CrowdSaleRefundVault.sol');
 const CrowdSale = artifacts.require('CrowdSale.sol');
 
 module.exports = async function (deployer) {
-    //todo Change wallet address. Should not be that same as contracts owner
     const walletAddress = '0x95b9c3afb6799509e8ea82faae4080e268b460e6';
 
     deployer.deploy(CrowdSaleRefundVault, walletAddress);
